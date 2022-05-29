7e786c93e2928f51a6770ce882255dc99ff675fa
hbsbashitha <hbsbashitha@gmail.com>
2022-05-29 17:45:24 +0530

    added some components from existing project

--- twilio-video-app-react-new/src/App.tsx
+++ twilio-video-app-react-new/src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import '../src/Assets/Styles/main.scss';
-import { Container, Row, Col } from 'react-bootstrap';
-import Routings from './components/Routings/Routings';
 
-import Twilio from './components/Twilio/Twilio';
-import TopNavbar from './components/Navbars/TopNavbar';
+import Routings from './components/Routings/Routings';
 
 export default function App() {
   return (
     <div className="App">
       <Routings />
-      <Twilio />
+      {/* <Twilio /> */}
     </div>
   );
 }
--- twilio-video-app-react-new/src/components/Routings/Routings.tsx
+++ twilio-video-app-react-new/src/components/Routings/Routings.tsx
@@ -1,40 +1,34 @@
 import * as React from 'react';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-    useParams
-} from "react-router-dom";
-import Twilio from "../Twilio/Twilio";
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
+import Twilio from '../Twilio/Twilio';
+import Dashboard from '../Pages/Student/Dashboard';
 
 const routings = [
-    {
-        path: "/",
-        exact: true,
-        main: () => <div> hi i am home page</div>,
-    },
-    {
-        path: "/twilio",
-        exact: true,
-        main: () => <Twilio />,
-    }
+  {
+    path: "/",
+    exact: true,
+    main: () => <Dashboard />,
+  },
+  {
+    path: "/twilio",
+    
+    main: () => <Twilio />,
+  },
+  {
+    path:"/c",
+    
+    main: () => <div>not found page</div>,
+  },
 ];
 
 export default function Routings() {
-    return (
-        <div className="MainPanel">
-            <Routes>
-                {routings.map((route, index) => (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        caseSensitive={route.exact}
-                        element={<route.main />}
-                    />
-                ))}
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="MainPanel">
+      <Routes>
+        {routings.map((route, index) => (
+          <Route key={index} path={route.path} caseSensitive={route.exact} element={<route.main />} />
+        ))}
+      </Routes>
+    </div>
+  );
 }
-
