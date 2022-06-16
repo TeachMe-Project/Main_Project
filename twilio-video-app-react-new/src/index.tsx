18a6e0c00ce5a867b483f09f35b4f7998ebd28a7
hbsbashitha <hbsbashitha@gmail.com>
2022-06-17 01:06:49 +0530

    set all routings and add all files from exsiting project

--- twilio-video-app-react-new/src/index.tsx
+++ twilio-video-app-react-new/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
 
 import { CssBaseline } from '@material-ui/core';
 import { MuiThemeProvider } from '@material-ui/core/styles';
@@ -16,51 +17,59 @@ import { ChatProvider } from './components/ChatProvider';
 import { VideoProvider } from './components/VideoProvider';
 import useConnectionOptions from './utils/useConnectionOptions/useConnectionOptions';
 import UnsupportedBrowserWarning from './components/UnsupportedBrowserWarning/UnsupportedBrowserWarning';
-import Twilio from "./components/Twilio/Twilio";
-import Dashboard from "./components/Pages/Student/Dashboard";
-import MyTeachers from "./components/Pages/Student/MyTeachers";
-import Settings from "./components/Pages/Student/Settings";
-import Course from "./components/Pages/Student/Course";
-import MyCourses from "./components/Pages/Student/MyCourses";
-import StudentProfile from "./components/Pages/Student/StudentProfile";
-
-const VideoApp = () => {
-  const { error, setError } = useAppState();
-  const connectionOptions = useConnectionOptions();
-
-  return (
-    <VideoProvider options={connectionOptions} onError={setError}>
-      <ErrorDialog dismissError={() => setError(null)} error={error} />
-      <ChatProvider>
-        <Twilio />
-      </ChatProvider>
-    </VideoProvider>
-  );
-};
+import Twilio from './components/Twilio/Twilio';
+import Dashboard from './components/Pages/Student/Dashboard';
+import MyTeachers from './components/Pages/Student/MyTeachers';
+import Settings from './components/Pages/Student/Settings';
+import Course from './components/Pages/Student/Course';
+import MyCourses from './components/Pages/Student/MyCourses';
+import StudentProfile from './components/Pages/Student/StudentProfile';
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    <UnsupportedBrowserWarning>
-      <Router>
-        <AppStateProvider>
-          <Routes>
-              <Route path="/" element={<App />} />
-              <Route path="/mycourses" element={<MyCourses />} />
-              <Route path="/myteachers" element={<MyTeachers />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/userprofile" element={<StudentProfile />} />
-              <Route path="/course" element={<Course />} />
-
-            <Route path="/twilio" element={<VideoApp />} />
-            <Route path="/room/:URLRoomName" element={<VideoApp />} />
-            <Route path="/login" element={<LoginPage />} />
-
-
-          </Routes>
-        </AppStateProvider>
-      </Router>
-    </UnsupportedBrowserWarning>
-  </MuiThemeProvider>,
+  // 👈️ deprecated starting React 18
+  <StrictMode>
+    <App />
+  </StrictMode>,
   document.getElementById('root')
 );
+
+// const VideoApp = () => {
+//     const { error, setError } = useAppState();
+//     const connectionOptions = useConnectionOptions();
+//
+//     return (
+//         <VideoProvider options={connectionOptions} onError={setError}>
+//             <ErrorDialog dismissError={() => setError(null)} error={error} />
+//             <ChatProvider>
+//                 <Twilio />
+//             </ChatProvider>
+//         </VideoProvider>
+//     );
+// };
+//
+// ReactDOM.render(
+//     <MuiThemeProvider theme={theme}>
+//         <CssBaseline />
+//         <UnsupportedBrowserWarning>
+//             <Router>
+//                 <AppStateProvider>
+//                     <Routes>
+//                         <Route path="/" element={<App />} />
+//                         <Route path="/mycourses" element={<MyCourses />} />
+//                         <Route path="/myteachers" element={<MyTeachers />} />
+//                         <Route path="/settings" element={<Settings />} />
+//                         <Route path="/userprofile" element={<StudentProfile />} />
+//                         <Route path="/course" element={<Course />} />
+//
+//                         <Route path="/twilio" element={<VideoApp />} />
+//                         <Route path="/room/:URLRoomName" element={<VideoApp />} />
+//                         <Route path="/login" element={<LoginPage />} />
+//
+//
+//                     </Routes>
+// //                 </AppStateProvider>
+// //             </Router>
+// //         </UnsupportedBrowserWarning>
+// //     </MuiThemeProvider>,
+//     document.getElementById('root')
+// );
