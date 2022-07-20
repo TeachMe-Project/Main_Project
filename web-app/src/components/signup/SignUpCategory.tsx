6f7bdf88a6671ab31bf316437c9d9e4ef8d4e2ab
Maneth <manethwijetunga@yahoo.com>
2022-07-20 16:18:16 +0530

    Signup Category page UIs done

--- web-app/src/components/signup/SignUpCategory.tsx
+++ web-app/src/components/signup/SignUpCategory.tsx
@@ -8,8 +8,9 @@ import {useAuth0} from "@auth0/auth0-react";
 import Footer from "../Home/footer/footer";
 
 const TEXTS = [
-    "Get started today and let Learning deliver online learning. So, you can focus on the quality of your tutoring.",
+    "Get started today and let Learning deliver your online learning. So, you can focus on the quality of your tutoring.",
     "Learning is an on-demand, easy-to-use, and cost-effective online learning solution for higher education institutions.",
+    "Want to have a comprehensive understanding of your kid's online tution classes? You have arrived at the right place!"
 ];
 
 const SignUpCategory = () => {
@@ -36,14 +37,14 @@ const SignUpCategory = () => {
                             <div
                                 className="d-lg-flex d-md-flex flex-column align-items-lg-start align-items-md-start justify-content-between mt-4 mb-md-4">
                                 <a href="/"
-                                   className="backBtn d-lg-flex text-black flex-row align-items-center fw-bolder text-decoration-none mb-lg-5 d-md-none d-sm-none">
-                                    <ImArrowLeft2 style={{marginRight: "4px"}}/> Go Back To Homepage
+                                   className="backBtn d-lg-flex flex-row align-items-center text-decoration-none mb-lg-5 d-md-none d-sm-none">
+                                    <ImArrowLeft2 style={{marginRight: "4px"}}/> Go Back
                                 </a>
                                 <img src={Images.logo} className="logo mt-lg-3" onClick={() => navigate('/')}
                                      alt="logo"/>
                             </div>
                             <h1 className="mt-lg-4 mt-md-5 mb-md-4 ms-md-2 Header mt-3">
-                                Sign Up In Learning
+                                Let's get started with Learning
                             </h1>
                             <p className="mt-lg-5 transformText ms-md-2 ms-sm-5">
                                 <TextTransition springConfig={presets.wobbly}>
@@ -54,11 +55,11 @@ const SignUpCategory = () => {
                     </Row>
                 </Col>
                 <Col lg={6} md={12} sm={12}
-                     className="d-flex flex-lg-row justify-content-lg-center justify-content-md-center mt-md-5 justify-content-md-center">
+                     className="d-flex flex-lg-row justify-content-lg-center justify-content-md-center mt-md-3 justify-content-md-center">
                     <Row className="categoryForm p-4">
                         <Col md={12} className="d-flex flex-column ms-2">
                             <img src={Images.bannerSignup} className="formImage" alt="form-category"/>
-                            <h1 className="text-center my-lg-2 mt-sm-2">Sign Up As</h1>
+                            <h1 className="text-center my-lg-2 mt-sm-2">Sign up as</h1>
                             <Button onClick={() => navigate('/signup/teacher')}
                                     className="userBtn mt-lg-4 mt-md-3 mx-auto  mt-3">Teacher</Button>
                             <Button onClick={() => navigate('/signup/institute')}
