09fc5c944c2993781a7a154ceb8ba27f5337be3f
jonathan <jonathan10dass@gmail.com>
2022-07-26 14:04:59 +0530

    finalized chat ui

--- twilio-video-app-react-new/src/components/Card/MessageCard.tsx
+++ twilio-video-app-react-new/src/components/Card/MessageCard.tsx
@@ -1,17 +1,38 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { CardDetails } from './CardDetails';
 
 type MessageCard = {
   teacher?: string;
   subject?: string;
-  lastmessage?: string;
+  // lastmessage?: string;
   image?: JSX.Element;
 };
 
 export const MessageCard: React.FC<MessageCard> = props => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    // 👇️ toggle
+    setIsActive(current => !current);
+
+    // 👇️ or set to true
+    // setIsActive(true);
+  };
+
   return (
-    <div className="CourseCard">
-      <div className="CardImage">{props.image}</div>
+    <div
+      className="ChatCard"
+      style={{
+        // backgroundColor: isActive ? 'salmon' : '',
+        // color: isActive ? 'white' : '',
+        // box-shadow: isActive ? '1px 7px 25px rgb(0 0 0 / 13%)' : '',
+        boxShadow: isActive ? '1px 7px 25px rgb(0 0 0 / 13%)' : '',
+        transform: isActive ? 'translateY(-4px)' : '',
+      }}
+      onClick={handleClick}
+    >
+      <div className="ChatCardImage">{props.image}</div>
       <div className="CardBody">
         <div className="teacherName">
           <CardDetails details={props.teacher} />
@@ -19,9 +40,9 @@ export const MessageCard: React.FC<MessageCard> = props => {
         <div className="teacherSubject">
           <CardDetails details={props.subject} />
         </div>
-        <div className="lastRow">
+        {/* <div className="lastRow">
           <CardDetails details={props.lastmessage} />
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- twilio-video-app-react-new/src/components/ChatBox/People/People.tsx
+++ twilio-video-app-react-new/src/components/ChatBox/People/People.tsx
@@ -2,37 +2,47 @@ import * as React from 'react';
 import { MessageCard } from '../../Card/MessageCard';
 
 export const People = () => {
+  // const [isActive, setIsActive] = useState(false);
+
+  // const handleClick = () => {
+  //   // 👇️ toggle
+  //   setIsActive(current => !current);
+
+  //   // 👇️ or set to true
+  //   // setIsActive(true);
+  // };
+
   return (
     <div className="displayChatters">
       <MessageCard
         subject="Mathematics"
         image={<img src={'/Images/Teachers/mr1.jpg'} />}
         teacher="Mr. Lasitha"
-        lastmessage="Noted"
+        // lastmessage="Noted"
       />
       <MessageCard
         subject="Science"
         image={<img src={'/Images/Teachers/ms1.jpg'} />}
         teacher="Mr. Nayana"
-        lastmessage="You need to cle..."
+        // lastmessage="You need to cle..."
       />
       <MessageCard
         subject="History"
         image={<img src={'/Images/Teachers/mr2.jpg'} />}
         teacher="Mr. Kamal"
-        lastmessage="Thank you sir"
+        // lastmessage="Thank you sir"
       />
       <MessageCard
         subject="Music"
         image={<img src={'/Images/Teachers/mr3.jpg'} />}
         teacher="Mr. Anura"
-        lastmessage="Sir can I have th..."
+        // lastmessage="Sir can I have th..."
       />
       <MessageCard
         subject="Art"
         image={<img src={'/Images/Teachers/mrs1.jpg'} />}
         teacher="Mrs. Shiromi"
-        lastmessage="I will upload the ..."
+        // lastmessage="I will upload the ..."
       />
     </div>
   );
