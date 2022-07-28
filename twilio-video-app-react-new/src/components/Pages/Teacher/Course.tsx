dc59220ddebd26ee620051f473ba7b1b73b6d05a
Avishka <62095876+Avishka217@users.noreply.github.com>
2022-07-28 14:34:47 +0530

    Commit editdetailspage
    Commit editdetailspage

--- twilio-video-app-react-new/src/components/Pages/Teacher/Course.tsx
+++ twilio-video-app-react-new/src/components/Pages/Teacher/Course.tsx
@@ -52,7 +52,7 @@ export const Course = () => {
             </div>
 
             <Tabs>
-              <div className="Details">
+              <div className="Details" style={{ marginTop: '50px' }}>
                 <Link to="/editdetails" className="link">
                   <Button
                     style={{
@@ -64,6 +64,7 @@ export const Course = () => {
                     Edit Details
                   </Button>
                 </Link>
+
                 <Details label="Subject" value="Mathematics" symbol=":" />
                 <Details label="Teacher" value="Mr. Lasitha Nuwan" symbol=":" />
                 <Details label="Grade" value="8" symbol=":" />
@@ -71,23 +72,26 @@ export const Course = () => {
                   label="Description"
                   value="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."
                 />
+                <Details label="Medium" value="Sinhala" symbol=":" />
                 <Details label="Monthly Payment" value="LKR 2500" symbol=":" />
                 <Details label="Started Date" value="2022-03-24" symbol=":" />
                 <Details label="Institute" value="Sigma Institute" symbol=":" />
                 <Details label="Duration" value="12 months" symbol=":" />
               </div>
-              <div className="Notes">
-                <Link className="link" to="/uploadnotes">
-                  <Button
-                    style={{
-                      float: 'right',
-                      marginRight: '44px',
-                    }}
-                  >
-                    Upload New notes
-                  </Button>
-                </Link>
 
+              <div className="Notes">
+                <Row>
+                  <Link className="link" to="/uploadnotes">
+                    <Button
+                      style={{
+                        float: 'right',
+                        marginRight: '44px',
+                      }}
+                    >
+                      Upload New notes
+                    </Button>
+                  </Link>
+                </Row>
                 <div className="noteContainer">
                   <Notes topic="Note for week 1" date="04-05-2022" />
                   <Notes topic="Note for week 2" date="04-05-2022" />
@@ -108,7 +112,7 @@ export const Course = () => {
                   </Button>
                 </Link>
 
-                <div className="homeworkContainer">
+                <div className="homeworkContainer" style={{ marginTop: '50px' }}>
                   <Homework topic="Homework for week 1" date="04-05-2022" />
                   <Homework topic="Homework for week 2" date="04-05-2022" />
                   <Homework topic="Homework for week 3" date="04-05-2022" />
@@ -118,7 +122,7 @@ export const Course = () => {
               </div>
 
               <div className="Students">
-                <div className="studentContainer">
+                <div className="studentContainer" style={{ marginTop: '50px' }}>
                   <table className="booking-table" id="view-booking">
                     <thead>
                       <tr className="booking-thead-second-tr">
@@ -222,17 +226,19 @@ export const Course = () => {
 
               <div className="Class-Schedules">
                 <div className="scheduleContainer">
-                  <Link className="link" to="/addextraclass">
-                    <Button
-                      style={{
-                        marginBottom: '20px',
-                        marginRight: '20px',
-                        float: 'right',
-                      }}
-                    >
-                      Add Extra Class
-                    </Button>
-                  </Link>
+                  <Row>
+                    <Link className="link" to="/addextraclass">
+                      <Button
+                        style={{
+                          marginBottom: '20px',
+                          marginRight: '20px',
+                          float: 'right',
+                        }}
+                      >
+                        Add Extra Class
+                      </Button>
+                    </Link>
+                  </Row>
                 </div>
                 <table className="booking-table" id="view-booking">
                   <thead>
