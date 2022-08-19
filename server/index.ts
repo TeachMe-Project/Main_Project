508e0f68b009cda00c68795e42dc735bb49f14b6
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-08-19 08:10:27 +0530

    some file errors changes

--- server/index.ts
+++ server/index.ts
@@ -21,6 +21,7 @@ import {notificationRouter} from "./route/notificationRoutes";
 import {authRouter} from "./route/authRoutes";
 import logger from "./utils/logger";
 import {contactRouter} from "./route/contactRoutes";
+import routes from './route/routes';
 
 const PORT = process.env.PORT ?? 8081;
 
@@ -61,7 +62,7 @@ app.use('/teacher', teacherRouter)
 app.use('/user', userRouter)
 app.use('/auth', authRouter)
 app.use('/contact', contactRouter)
-
+app.use('/api',routes)
 app.use('/create-checkout-session',paymentGatewayRouter)
 
 
--- server/route/routes.ts
+++ server/route/routes.ts
@@ -2,9 +2,9 @@ import {Express, Request, Response} from 'express';
 import logger from "../utils/logger";
 
 const routes = (app: Express) => {
-    app.get("/api/v1/check", (req: Request, res: Response) => {
+    app.get("/check", (req: Request, res: Response) => {
         logger.info("Checking:","Ok","200");
-        res.status(200).send('Ok')
+        res.status(200).send('Ok All Are Working')
     });
 }
 
