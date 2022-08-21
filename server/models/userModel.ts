6614b78a8e2a9e817ca52cfd4b42b905c46ab0f4
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-08-21 12:11:07 +0530

    change logo

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -4,6 +4,7 @@ const userSchema = Joi.object({
     user_id: Joi.string().min(1).required(),
     username: Joi.string().min(1).required(),
     type: Joi.string().min(1).required(),
+    isActive:Joi.required(),
     profile_image: Joi.string().min(1).required()
 });
 
