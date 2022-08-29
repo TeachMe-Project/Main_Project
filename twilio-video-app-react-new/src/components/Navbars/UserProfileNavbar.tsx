feb802c06febbd5742d170e3cc5150515a7360eb
jonathan <jonathan10dass@gmail.com>
2022-08-29 15:42:11 +0530

    user backend TopNavbar I

--- twilio-video-app-react-new/src/components/Navbars/UserProfileNavbar.tsx
+++ twilio-video-app-react-new/src/components/Navbars/UserProfileNavbar.tsx
@@ -8,45 +8,17 @@ import { NavItem } from 'react-bootstrap';
 
 type UserProfileNavbar = {
   name: string;
-  image?: HTMLImageElement;
+  image?: string;
 };
 
 export const UserProfileNavbar = (props: UserProfileNavbar) => {
-  const baseURL = 'https://learnx.azurewebsites.net/user/:id';
-  const [profilePic, setProfilePic] = useState<any[]>([]);
-
-  useEffect(() => {
-    axios
-      .get(baseURL)
-      .then((res: AxiosResponse) => {
-        res.data.map((item: any) => {
-          setProfilePic(prevState => [
-            ...prevState,
-            {
-              profile_pic: item.profile_pic,
-            },
-          ]);
-        });
-        console.log(profilePic);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
 
   return (
     <div className="UserProfileNavbar">
       <div className="UserProfileButton">
         <Link to="/userprofile" className="link">
           <div className="UserImg">
-            {/* {profilePic.map((item: any) => {
-              return (
-                <img src={item.profile_pic} />
-              );
-            }
-            )} */}
-            <img src={'https://learninggp2.blob.core.windows.net/images/student.png'} />
-            {/*<img src={require("../../Assets/Images/testimg2.jpeg")} />*/}
+            <img src={props.image} />
           </div>
           <div className="Name">{props.name}</div>
         </Link>
